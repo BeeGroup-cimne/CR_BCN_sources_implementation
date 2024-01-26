16bb32e1470b3ab7d54de981edb12806eef1fd87
jose <manoloainsa@gmail.com>
2024-01-26 14:59:17 +0100

    harmonizer changes

--- sources/Inspire/harmonizer/Inspire_mapping.py
+++ sources/Inspire/harmonizer/Inspire_mapping.py
@@ -9,6 +9,7 @@ from slugify import slugify
 import settings
 import morph_kgc
 import pandas as pd
+import numpy as np
 import rdflib
 from utils.data_transformations import fuzzy_dictionary_match, fuzz_params
 
@@ -88,7 +89,7 @@ def df_to_formatted_json(df, sep="."):
 
 def get_address_street_name(df_point):
 
-    address_street_names_df = pd.read_csv("data/Inspire/address/addressTh08900.csv", usecols = ['gml_id','text'])
+    address_street_names_df = pd.read_csv("data/Inspire/address/08_address_th.csv", usecols = ['gml_id','text'])
     address_street_names_df["gml_id"] = address_street_names_df['gml_id'].apply(lambda x: x.split('ES.SDGC.TN.')[1])
     df_point["properties.merge_gml_id"] = df_point['properties.gml_id'].apply(lambda x: '.'.join(x.split('ES.SDGC.AD.')[1].split('.')[:3]))
     merge_df = pd.merge(df_point, address_street_names_df, left_on="properties.merge_gml_id", right_on="gml_id")
@@ -102,10 +103,10 @@ def coordinate_to_string(json_data):
         print(item['geometry']['coordinates'])
 def harmonize_buildings_static(data, **kwargs):
     config = utils.utils.read_config(settings.conf_file)
-    df_buildings = pd.DataFrame(data)
+    # df_buildings = pd.DataFrame(data)
 
-    centroid_data = json.load(open("data/Inspire/download/building/temp/08900_buildings_geom_centroid.geojson"))
-    geojson_data = json.load(open("data/Inspire/download/building/temp/08900_buildings_geom.geojson"))
+    centroid_data = json.load(open("data/Inspire/building/08900_buildings_geom_centroid.geojson"))
+    geojson_data = json.load(open("data/Inspire/building/08900_buildings_geom.geojson"))
 
     with open("data/Inspire/building/data.json", "w") as d_file:
         json.dump({"buildings": {"point": centroid_data['features'], "geojson": geojson_data['features']}}, d_file)
@@ -124,25 +125,39 @@ def harmonize_buildings_static(data, **kwargs):
         lambda x: x.split('_')[0].split('BU.')[1])
     df_geojson['geometry.coordinates'] = df_geojson['geometry.coordinates'].apply(lambda x: ' '.join([str(item) for item in x]))
 
+    # Number of rows per split
+    rows_per_split = 10000
 
-    with open("sources/Inspire/harmonizer/temp.json", "w") as d_file:
-        json.dump({"buildings": {"point": df_to_formatted_json(df_point, sep="."), "geojson": df_to_formatted_json(df_geojson, sep=".")}}, d_file)
+    # Number of splits
+    num_splits = len(df_point) // rows_per_split
 
-    g_rdflib = morph_kgc.materialize(morph_config.format(d_file=d_file.name))
-    os.unlink("sources/Inspire/harmonizer/temp.json")
-    neo = GraphDatabase.driver(**config['neo4j'])
-    content = g_rdflib.serialize(format="ttl")
-    content = content.replace('\\"', "&apos;")
-    content = content.replace("'", "&apos;")
-    with neo.session() as s:
-        response = s.run(f"""CALL n10s.rdf.import.inline('{content}','Turtle')""")
-        print(response.single())
+    # Split the DataFrame into parts
+    df_points_splits = np.array_split(df_point, num_splits)
+    df_geojson_splits = np.array_split(df_geojson, num_splits)
+
+    # Load to Neo4J
+    for i, split in enumerate(df_points_splits):
+        print(f"Split {i + 1}:\n{split}\n")
+        with open("sources/Inspire/harmonizer/temp.json", "w") as d_file:
+            json.dump({"buildings": {"point": df_to_formatted_json(df_points_splits[i], sep="."),
+                                     "geojson": df_to_formatted_json(df_geojson_splits[i], sep=".")}}, d_file)
+
+        g_rdflib = morph_kgc.materialize(morph_config.format(d_file=d_file.name))
+        os.unlink("sources/Inspire/harmonizer/temp.json")
+        neo = GraphDatabase.driver(**config['neo4j'])
+        content = g_rdflib.serialize(format="ttl")
+        content = content.replace('\\"', "&apos;")
+        content = content.replace("'", "&apos;")
+        with neo.session() as s:
+            response = s.run(f"""CALL n10s.rdf.import.inline('{content}','Turtle')""")
+            print(response.single())
+
 def harmonize_buildings_space_static(data, **kwargs):
     config = utils.utils.read_config(settings.conf_file)
     # df_buildings = pd.DataFrame(data)
 
-    centroid_data = json.load(open("data/Inspire/buildingSpace/08011buildingSpaceCentroid.geojson"))
-    geojson_data = json.load(open("data/Inspire/buildingSpace/08011buildingSpaceGeoJSON.geojson"))
+    centroid_data = json.load(open("data/Inspire/buildingSpace/08900_building_part_geom_centroid.geojson"))
+    geojson_data = json.load(open("data/Inspire/buildingSpace/08900_building_part_geom.geojson"))
 
     with open("data/Inspire/buildingSpace/data.json", "w") as d_file:
         json.dump({"buildingsSpaces": {"point": centroid_data['features'], "geojson": geojson_data['features']}}, d_file)
@@ -158,32 +173,38 @@ def harmonize_buildings_space_static(data, **kwargs):
     df_geojson = pd.json_normalize(json_data['buildingsSpaces']['geojson'])
     df_geojson['geometry.coordinates'] = df_geojson['geometry.coordinates'].apply(lambda x: ' '.join([str(item) for item in x]))
 
-    with open("sources/Inspire/harmonizer/temp.json", "w") as d_file:
-        json.dump({"buildingsSpaces": {"point": df_to_formatted_json(df_point, sep="."), "geojson": df_to_formatted_json(df_geojson, sep=".")}}, d_file)
+    # Number of rows per split
+    rows_per_split = 10000
 
-    g_rdflib = morph_kgc.materialize(morph_config.format(d_file=d_file.name))
-    os.unlink("sources/Inspire/harmonizer/temp.json")
-    neo = GraphDatabase.driver(**config['neo4j'])
-    content = g_rdflib.serialize(format="ttl")
-    content = content.replace('\\"', "&apos;")
-    content = content.replace("'", "&apos;")
-    with neo.session() as s:
-        response = s.run(f"""CALL n10s.rdf.import.inline('{content}','Turtle')""")
-        print(response.single())
+    # Number of splits
+    num_splits = len(df_point) // rows_per_split
+
+    # Split the DataFrame into parts
+    df_points_splits = np.array_split(df_point, num_splits)
+    df_geojson_splits = np.array_split(df_geojson, num_splits)
+
+    for i, split in enumerate(df_points_splits):
+        print(f"Split {i + 1}:\n{split}\n")
+        with open("sources/Inspire/harmonizer/temp.json", "w") as d_file:
+            json.dump({"buildingsSpaces": {"point": df_to_formatted_json(df_points_splits[i], sep="."),
+                                           "geojson": df_to_formatted_json(df_geojson_splits[i], sep=".")}}, d_file)
+        g_rdflib = morph_kgc.materialize(morph_config.format(d_file=d_file.name))
+        os.unlink("sources/Inspire/harmonizer/temp.json")
+        content = g_rdflib.serialize(format="ttl")
+        content = content.replace('\\"', "&apos;")
+        content = content.replace("'", "&apos;")
+        neo = GraphDatabase.driver(**config['neo4j'])
+        with neo.session() as s:
+            response = s.run(f"""CALL n10s.rdf.import.inline('{content}','Turtle')""")
+            print(response.single())
 
 def harmonize_address_static(data, **kwargs):
-    # config = utils.utils.read_config(settings.conf_file)
+    config = utils.utils.read_config(settings.conf_file)
     # df_buildings = pd.DataFrame(data)
 
-    import json
-    import tempfile
-    import os
-    import morph_kgc
-    import pandas as pd
-    import rdflib
     morph_config = '\n[DataSource1]\nmappings:data/Inspire/address/mapping.yaml\nfile_path: {d_file}\n'
 
-    centroid_data = json.load(open("data/Inspire/address/address08011.geojson"))
+    centroid_data = json.load(open("data/Inspire/address/08900_address_geom.geojson"))
 
     with open("data/Inspire/address/data.json", "w") as d_file:
         json.dump({"address": {"point": centroid_data['features']}}, d_file)
