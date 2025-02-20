02413c9aa934dc7fafc78bfc75af0c70e7192bfb
Miriam <miriam.mendez.s@gmail.com>
2025-02-20 19:53:51 +0100

    script updated for era5land data

--- sources/Weather_Scaling_Up/load.py
+++ sources/Weather_Scaling_Up/load.py
@@ -14,14 +14,15 @@ def harmonize_endesa(config, **kwargs):
     df_original["weatherId"] = df_original['weatherId'].apply(lambda x: hashlib.sha256(x).hexdigest())
     df_original[['latitude','longitude']] = df_original['weatherStation'].str.split('_', expand=True).astype(float)
 
-    df = df_original.copy()
-    df.drop_duplicates(subset=['weatherStation'],inplace=True,keep='first')
 
     # Load to Neo4j
+    df = df_original.copy()
+    df.drop_duplicates(subset=['weatherStation'],inplace=True,keep='first')
     documents = {"weather": df.to_dict(orient='records')}
     beelib.beetransformation.map_and_save(documents, morph_config, config)
 
 
+    # Load to HBase
     freq = "P1M"
     user = "CRBCN"
 
@@ -30,37 +31,36 @@ def harmonize_endesa(config, **kwargs):
 
     df_original['relativeHumidity'] = df_original['relativeHumidity'].astype(float) * 100
     df_original['airTemperature'] = df_original['airTemperature'].astype(float) 
-    df_original['forecastingTime'] = pd.to_datetime(df_original['forecastingTime'], format='%Y-%m-%dT%H:%M:%S.%f', errors='coerce')
+    # df_original['forecastingTime'] = pd.to_datetime(df_original['forecastingTime'], format='%Y-%m-%dT%H:%M:%S.%f', errors='coerce')
     df_original['time'] = pd.to_datetime(df_original['time'], format='%Y-%m-%dT%H:%M:%S.%f', errors='coerce')
     
-    df_original['forecastingTime'] = df_original['forecastingTime'].astype(int) // 10 ** 9
+    # df_original['forecastingTime'] = df_original['forecastingTime'].astype(int) // 10 ** 9
     df_original['start'] = df_original['time'].astype(int) // 10 ** 9
     df_original['end'] = df_original['time'].apply(lambda x: (x + pd.offsets.MonthEnd(0))).astype(int) // 10 ** 9
     df_original["bucket"] = (df_original['start'].apply(int) // ts_buckets) % buckets
     df_original['isReal'] = False
 
-    # # Hbase
     hbase_conn = config['hbase_store_raw_data']
-    weather_device_table = f"harmonized_online_DryBulbTemperatureScaling_100_SUM_{freq}_{user}"
+    weather_device_table = f"harmonized_online_DryBulbTemperatureDownScaling_100_SUM_{freq}_{user}"
     save_to_hbase(df_original.to_dict(orient="records"), weather_device_table, hbase_conn,
                   [("info", ['end', 'isReal']), ("v", ['airTemperature'])],
-                  row_fields=['bucket', 'weatherId', 'forecastingTime','start'])
+                  row_fields=['bucket', 'weatherId', 'start'])
 
-    weather_device_table = f"harmonized_online_RelativeHumidityScaling_100_SUM_{freq}_{user}"
+    weather_device_table = f"harmonized_online_RelativeHumidityDownScaling_100_SUM_{freq}_{user}"
     save_to_hbase(df_original.to_dict(orient="records"), weather_device_table, hbase_conn,
                   [("info", ['end', 'isReal']), ("v", ['relativeHumidity'])],
-                  row_fields=['bucket', 'weatherId', 'forecastingTime' 'start'])
+                  row_fields=['bucket', 'weatherId',  'start'])
 
 
-    weather_period_table = f"harmonized_batch_DryBulbTemperatureScaling_100_SUM_{freq}_{user}"
+    weather_period_table = f"harmonized_batch_DryBulbTemperatureDownScaling_100_SUM_{freq}_{user}"
     save_to_hbase(df_original.to_dict(orient="records"), weather_period_table, hbase_conn,
                   [("info", ['end', 'isReal']), ("v", ['airTemperature'])],
-                  row_fields=['bucket', 'start', 'forecastingTime', 'weatherId'])
+                  row_fields=['bucket', 'start',  'weatherId'])
 
-    weather_period_table = f"harmonized_batch_RelativeHumidityScaling_100_SUM_{freq}_{user}"
+    weather_period_table = f"harmonized_batch_RelativeHumidityDownScaling_100_SUM_{freq}_{user}"
     save_to_hbase(df_original.to_dict(orient="records"), weather_period_table, hbase_conn,
                   [("info", ['end', 'isReal']), ("v", ['relativeHumidity'])],
-                  row_fields=['bucket', 'start', 'forecastingTime', 'weatherId'])
+                  row_fields=['bucket', 'start', 'weatherId'])
 
 config = beelib.beeconfig.read_config('/media/mmartinez/WINDOWS/TFM/BEEGroup/config.json')
 harmonize_endesa(config)
